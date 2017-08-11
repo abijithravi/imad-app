4d51ead7f74d2b7ae3f8acb94b369627f8fdb0a9
abijithravi <30771851+abijithravi@users.noreply.github.com>
2017-08-11 18:57:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,13 @@ var submit  = doucument.getElementById('submit_btn');
 submit.onClick = function(){
     
     
+    var comment = ["com1","com2","com3"];
+    var list ='';
+    for(i=0 ; i<=comment.length ; i++){
+        list += '<li>' + comment[i] + '</li>';
+        
+    }
+var ul = getElementbyId('comlist');
+ul.inertHTML = list;
     
 };
